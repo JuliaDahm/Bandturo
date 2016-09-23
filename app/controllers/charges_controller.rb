1f2e4464056369d3f65a090a372cf2b5c340999a
Julia Dahm <jukabrown@gmail.com>
2016-09-23 13:10:21 -0400

    moved around amount variable. attempt to resolve empty variable problem.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,14 +4,14 @@ class ChargesController < ApplicationController
   end 
 
   def create
-    @amount = params[:amount]
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
       :source  => params[:stripeToken]
     )
-
+gi
     charge = Stripe::Charge.create(
+      @amount = params[:amount]
       :customer    => customer.id,
       :amount      => @amount,
       :description => 'Bandturo Customer',
