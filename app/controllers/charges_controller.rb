9b12996f475912b3d4501d7dd874bf5a92e42f1d
Julia Dahm <jukabrown@gmail.com>
2016-09-26 17:26:10 -0400

    changed form id and the variables that store them

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,7 +5,21 @@ class ChargesController < ApplicationController
 
   def create
 
-    @plan = params[:plan_chosen]
+    @amount = case params[:plan_chosen]
+      when 0 then 4900 
+      when 1 then 9900
+      when 2 then 19900
+      else
+        raise 'Plan not supported'
+      end
+
+    @description = case params[:plan_chosen]
+      when 0 then ''
+      when 1 then ''
+      when 2 then 'Cul-de-sac'
+      else 
+        raise 'plan not supported'
+      end 
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
@@ -15,12 +29,16 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
-      :description => 'Bandturo Customer',
+      :description => @description,
       :currency    => 'usd'
     )
 
+  if charge.save
+    redirect_to thanks_index_path
+  else 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
 end
